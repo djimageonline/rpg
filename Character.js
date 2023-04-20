1e0db637e925d28bd1d7a14bbe0566a6565ec9a4
Johnny Proano <jproano22@yahoo.com>
2023-04-20 17:35:43 -0400

    added getDicePlaceholderHtml function in utils and imported to character constructor creting this.array

--- Character.js
+++ Character.js
@@ -1,7 +1,9 @@
-import { getDiceRollArray } from "./utils.js";
+import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
 function Character(data) {
   Object.assign(this, data);
+
+  this.diceArray = getDicePlaceholderHtml(this.diceCount);
 
   this.getDiceHtml = function (diceCount) {
     return getDiceRollArray(diceCount)
@@ -20,7 +22,7 @@ function Character(data) {
           <img class="avatar" src="${avatar}" />
           <div class="health">health: <b> ${health} </b></div>
           <div class="dice-container">    
-              ${diceHtml}
+              ${this.diceArray}
           </div>
       </div>`;
   };
