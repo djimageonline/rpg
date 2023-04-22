6618414df5328dc9917015b5cf5b17e6491ba49e
Johnny Proano <jproano22@yahoo.com>
2023-04-21 22:54:12 -0400

    added get Percentage, checkeld

--- Character.js
+++ Character.js
@@ -1,7 +1,10 @@
 import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 
+const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth;
+
 function Character(data) {
   Object.assign(this, data);
+  this.maxHealth = this.health;
 
   this.diceArray = getDicePlaceholderHtml(this.diceCount);
 
@@ -14,22 +17,35 @@ function Character(data) {
     const totalAttackScore = attackScoreArray.reduce((total, num) => total + num);
     this.health -= totalAttackScore;
     if (this.health <= 0) {
-      this.health = 0;
       this.dead = true;
+      this.health = 0;
     }
   };
 
-  this.getCharacterHtml = function () {
-    const { elementId, name, avatar, health, diceCount, diceArray } = this;
+  this.getHealthBarHtml = function () {
+    const percent = getPercentage(this.health, this.maxHealth);
 
-    return `<div class="character-card">
-          <h4 class="name"> ${name} </h4>
-          <img class="avatar" src="${avatar}" />
-          <div class="health">health: <b> ${health} </b></div>
-          <div class="dice-container">    
-              ${diceArray}
-          </div>
-      </div>`;
+    return `
+        <div class="health-bar-outer">
+            <div class="health-bar-inner ${percent < 26 ? "danger" : ""} " 
+            style="width: ${percent}%;">
+            </div>
+        </div>`;
+  };
+
+  this.getCharacterHtml = function () {
+    const { elementId, name, avatar, health, diceCount } = this;
+    const healthBar = this.getHealthBarHtml();
+    return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
+                <div class="dice-container">
+                    ${this.diceArray}
+                </div>
+            </div>`;
   };
 }
 
